3c657533f6725bb4b6a7277326b55c88b54d3b5e
Jonas Evangelista <jonas.eos@outlook.com>
2019-06-26 21:41:46 -0300

    Atualizaçao mapa.c
    * Adicionado procedimento preencherVendas

--- mapa.c
+++ mapa.c
@@ -9,6 +9,18 @@ enum indexes {
   COLUNA = 6
 };
 
+void preencherVendas() {
+  int semana, diaDaSemana;
+  double vendas[LINHA][COLUNA];
+  for (semana = 0; semana < LINHA; semana++) {
+    printf("Semana %i\n", semana);
+    for (diaDaSemana = 0; diaDaSemana < 6; diaDaSemana++) {
+      prinf("Dia: %i", diaDaSemana);
+      scanf("%lf", &vendas[semana][diaDaSemana]);
+    }  // diaDaSemana
+  }    // Semana
+}
+
 int main() {
   return 0;
 }
