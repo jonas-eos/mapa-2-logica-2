bd34a69300a613b39ad4034f923d4803359a6c93
Jonas Evangelista <jonas.eos@outlook.com>
2019-06-26 21:59:57 -0300

    Atualizaçao no mapa.c
    * Adicionado comentarios descrevendo sobre o procedimento
    * Adicionado chamador do procedimento preencherVendas no programa
    principal

---
+++ mapa.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+enum indexes {
+
+  /// Usado como guia para semanas
+  LINHA = 4,
+
+  /// Usado como guia para os dias da semana
+  COLUNA = 6
+};
+
+//******************************************************************************
+// Descrição
+// - preenche uma matriz de vendas.
+// Dependência
+// - const LINHA
+// - const COLUNA
+void preencherVendas() {
+  int semana, diaDaSemana;
+  double vendas[LINHA][COLUNA];
+  for (semana = 0; semana < LINHA; semana++) {
+    printf("Semana %i\n", semana);
+    for (diaDaSemana = 0; diaDaSemana < 6; diaDaSemana++) {
+      printf("Dia: %i", diaDaSemana);
+      scanf("%lf", &vendas[semana][diaDaSemana]);
+    }  // diaDaSemana
+  }    // Semana
+}
+
+//******************************************************************************
+// Programa principal
+int main() {
+  preencherVendas();
+  return 0;
+}
