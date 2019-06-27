76bba76624038188729d404858196a698a662100
Jonas Evangelista <jonas.eos@outlook.com>
2019-06-26 21:35:40 -0300

    Atualizaçao no mapa.c
    * Adicionado constantes linha e coluna

--- mapa.c
+++ mapa.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+enum indexes {
+
+  /// Usado como guia para semanas
+  LINHA = 4,
+
+  /// Usado como guia para os dias da semana
+  COLUNA = 6
+};
+
 int main() {
   return 0;
 }
